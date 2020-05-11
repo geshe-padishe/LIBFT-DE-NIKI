6bef284b1e422fc269166f0cf27f15c0ceffdb7e
ngenadie <ngenadie@student.42.fr>
2020-05-11 14:28:25 +0200

    maybe fixed calloc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -13,27 +13,34 @@ void *ft_calloc(size_t nbelm, size_t size)
 	if (ptr == NULL)
 		return (NULL);
 	str = ptr;
-	if (size == 0)
+	if (nbelm == 0)
 		str[i] = 0;
 	while (i < nbelm)
 		str[i++] = 0;
 	return (ptr);
 }
 
-int main(int argc, char **argv)
-{
-	int i = 0;
-	char *ptr;
-	char *ptr2;
-
-	ptr = (char*)calloc(atoi(argv[1]), atoi(argv[2]));
-	ptr2 = (char*)ft_calloc(atoi(argv[1]), atoi(argv[2]));
-
+//int main(int argc, char **argv)
+//{
+//	int size = 8539;
+//	void * d1 = ft_calloc(size, sizeof(int));
+//	void * d2 = calloc(size, sizeof(int));
+//	
+//	if (memcmp(d1, d2, size * sizeof(int)))
+//		printf("failed");
+//	free(d1);
+//	free(d2);
+//	printf("success");
+//	char *ptr;
+//	char *ptr2;
+//	argc = 2;
+//	ptr = (char*)calloc(atoi(argv[1]), atoi(argv[2]));
+//	ptr2 = (char*)ft_calloc(atoi(argv[1]), atoi(argv[2]));
+//
 //	ptr[0] = 'W';
 //	ptr2[0] = 'W';
-
-	
-	//printf("real => %s\n", ptr);
+//
+//	
+//	printf("real => %s\n", ptr);
 //	printf("meme => %s\n", ptr2);
-
-}
+//}
