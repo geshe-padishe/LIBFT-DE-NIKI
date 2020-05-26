950bb91650df2eb68a58e9bc75a73f9618266433
ngenadie <ngenadie@student.42.fr>
2020-05-26 14:57:08 +0200

    .h dinclusion protect

--- inc/libft.h
+++ inc/libft.h
@@ -1,3 +1,6 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
 #include <stdio.h>
 
 void	*ft_memset(void *s, int c, size_t n);
@@ -35,3 +38,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+#endif
