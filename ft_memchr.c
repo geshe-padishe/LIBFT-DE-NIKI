2c2beb60cadb4b30adc22d00e681d35515b2bbb4
ngenadie <ngenadie@student.42.fr>
2020-07-09 15:58:02 +0200

    memchr adapt mac

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,33 +6,36 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/28 18:41:56 by user42            #+#    #+#             */
-/*   Updated: 2020/05/28 18:41:56 by user42           ###   ########.fr       */
+/*   Updated: 2020/07/09 15:31:29 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const char *src = s;
+	const char *src = (const char *)s;
+	size_t i;
 
-	while (n-- > 0)
-		if (*src++ == c)
-			return ((void*)--src);
+	i = 0;
+	while (i < n)
+	{
+		if (src[i] == c)
+			return ((void*)&src[i]);
+		i++;
+	}
 	return (NULL);
 }
-/*
-**int main(int argc, char **argv)
-**{
-**	char str[10] = "bussjuggs";
-**	const char *ptr = str;
-**	char *ptr2;
-**	char c = argv[1][0];
-**
-**	ptr2 = ft_memchr(ptr, c, 9);
-**	printf("me => %s\n", ptr2);
-**	ptr2 = memchr(ptr, c, 9);
-**	printf("real => %s\n", ptr2);
-**}
-*/
+
+int main(int argc, char **argv)
+{
+	char *ptr2;
+
+	ptr2 = ft_memchr(argv[1], argv[2][0], atoi(argv[3]));
+	printf("meme => %s\n", ptr2);
+	ptr2 = memchr(argv[1], argv[2][0], atoi(argv[3]));
+	printf("real => %s\n", ptr2);
+}
+
